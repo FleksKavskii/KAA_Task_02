1bfe743cfc52d610e7cd6f1cce95a50ddbddcadc
FleksKavskii <66283133+FleksKavskii@users.noreply.github.com>
2020-11-16 17:54:57 +0300

    KAA_Task_02

---
+++ KAA_Task_02/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace KAA_Task_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите exit: ");
+            string word = Console.ReadLine();
+            while (!word.Contains("exit"))
+            {
+                if (word != "exit")
+                {
+                    Console.Write("Повторите попытку ");
+                    word = Console.ReadLine();
+                }
+            }
+
+        }
+    }
+}
